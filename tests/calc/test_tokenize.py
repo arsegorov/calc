59fc62e72e276fe25e1d712f52b9c2f6ea3719ff
Arseny Egorov <arsegorov@gmail.com>
2022-07-02 16:51:29 -0700

    `_tokenize` tests

--- tests/calc/test_tokenize.py
+++ tests/calc/test_tokenize.py
@@ -1,5 +1,143 @@
+import pytest
 from calc.calc import Calc
+from calc.op import Bracket, Op
 
 
-def test():
-    assert True
+def test_float_wo_exp(calc_instance: Calc):
+    calc_instance.input = "1.2"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(1.2, 0, 3)]
+
+def test_float_w_exp(calc_instance: Calc):
+    calc_instance.input = "0.12e1"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(1.2, 0, 6)]
+
+def test_float_w_exp_negative(calc_instance: Calc):
+    calc_instance.input = "1.2e-1"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(0.12, 0, 6)]
+
+def test_float_w_exp_positive(calc_instance: Calc):
+    calc_instance.input = ".12e+1"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(1.2, 0, 6)]
+
+def test_float_w_exp_upper(calc_instance: Calc):
+    calc_instance.input = "1.2E-1"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(0.12, 0, 6)]
+
+def test_float_is_int_wo_exp(calc_instance: Calc):
+    calc_instance.input = "12."
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(12, 0, 3)]
+
+def test_float_is_int_w_exp(calc_instance: Calc):
+    calc_instance.input = "1.2e2"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(120, 0, 5)]
+
+def test_float_only_decimal_part(calc_instance: Calc):
+    calc_instance.input = ".2"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(0.2, 0, 2)]
+
+def test_float_only_decimal_point(calc_instance: Calc):
+    with pytest.raises(ValueError, match=r"unexpected text at character 1: '\.'"):
+        calc_instance.input = "."
+        calc_instance._tokenize()
+
+def test_dec_int(calc_instance: Calc):
+    calc_instance.input = "12"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(12, 0, 2)]
+
+def test_bin(calc_instance: Calc):
+    calc_instance.input = "0b11"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(3, 0, 4)]
+
+def test_bin_invalid(calc_instance: Calc):
+    with pytest.raises(ValueError, match=r"invalid literal for int\(\) with base 2"):
+        calc_instance.input = "0b12"
+        calc_instance._tokenize()
+
+def test_oct(calc_instance: Calc):
+    calc_instance.input = "0o17"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(15, 0, 4)]
+
+def test_oct_invalid(calc_instance: Calc):
+    with pytest.raises(ValueError, match=r"invalid literal for int\(\) with base 8"):
+        calc_instance.input = "0o19"
+        calc_instance._tokenize()
+
+def test_oct_w_exp_invalid(calc_instance: Calc):
+    with pytest.raises(ValueError, match=r"invalid literal for int\(\) with base 8"):
+        calc_instance.input = "0o77E+1"
+        calc_instance._tokenize()
+
+def test_hex(calc_instance: Calc):
+    calc_instance.input = "0xff"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(255, 0, 4)]
+
+def test_hex_invalid(calc_instance: Calc):
+    with pytest.raises(ValueError, match=r"invalid literal for int\(\) with base 16"):
+        calc_instance.input = "0xfg"
+        calc_instance._tokenize()
+
+def test_dec_int_w_exp(calc_instance: Calc):
+    calc_instance.input = "12e1"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(120, 0, 4)]
+
+def test_dec_int_w_exp_negative(calc_instance: Calc):
+    calc_instance.input = "12e-1"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [(1.2, 0, 5)]
+
+def test_ops(calc_instance: Calc):
+    calc_instance.input = "+ - * ** / // %"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [
+        (Op.ADD, 0, 1),
+        (Op.SUB, 2, 3),
+        (Op.MULT, 4, 5),
+        (Op.EXP, 6, 8),
+        (Op.DIV, 9, 10),
+        (Op.DIV_INT, 11, 13),
+        (Op.MOD, 14, 15),
+    ]
+
+def test_brackets(calc_instance: Calc):
+    calc_instance.input = "()[]{}"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [
+        (Bracket.P_OPEN, 0, 1),
+        (Bracket.P_CLOSE, 1, 2),
+        (Bracket.S_OPEN, 2, 3),
+        (Bracket.S_CLOSE, 3, 4),
+        (Bracket.C_OPEN, 4, 5),
+        (Bracket.C_CLOSE, 5, 6),
+    ]
+
+def test_integration(calc_instance: Calc):
+    calc_instance.input = "2/[12 - 4*(1.2E-3 ** 0.5)]"
+    calc_instance._tokenize()
+    assert calc_instance._tokens == [
+        (2, 0, 1),
+        (Op.DIV, 1, 2),
+        (Bracket.S_OPEN, 2, 3),
+        (12, 3, 5),
+        (Op.SUB, 6, 7),
+        (4, 8, 9),
+        (Op.MULT, 9, 10),
+        (Bracket.P_OPEN, 10, 11),
+        (0.0012, 11, 17),
+        (Op.EXP, 18, 20),
+        (0.5, 21, 24),
+        (Bracket.P_CLOSE, 24, 25),
+        (Bracket.S_CLOSE, 25, 26),
+    ]
