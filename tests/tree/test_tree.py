06846e56b112c07415764de90f8103d318d1bbb3
Arseny Egorov <arsegorov@gmail.com>
2022-07-02 17:10:53 -0700

    Add partial `__init__` tests

--- tests/tree/test_tree.py
+++ tests/tree/test_tree.py
@@ -6,6 +6,18 @@ def test_init():
     assert root.left == None
     assert root.right == None
 
+def test_init_left():
+    left = Tree()
+    root = Tree(left)
+    assert root.left == left
+    assert root.right == None
+
+def test_init_right():
+    right = Tree()
+    root = Tree(right=right)
+    assert root.left == None
+    assert root.right == right
+
 def test_getters():
     left = Tree()
     right = Tree()
