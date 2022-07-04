8f5caaff7cd2d1af482a58af4e67549ddb216024
Arseny Egorov <arsegorov@gmail.com>
2022-07-03 19:28:46 -0700

    Implement `_group_by_brackets`

--- calc/calc.py
+++ calc/calc.py
@@ -1,7 +1,7 @@
 import re
 from numbers import Number
 from string import digits
-from typing import List
+from typing import List, Tuple
 
 if __name__ == "__main__":
     from op import Bracket, Op
@@ -9,6 +9,11 @@ if __name__ == "__main__":
 else:
     from .op import Bracket, Op
     from .tree import Tree
+
+
+Token = Tuple[Bracket | Number | Op, int, int]
+TokensList = List[Token]
+GroupedTokensList = List[Tuple[Number | Op, int, int] | "GroupedTokensList"]
 
 
 class OpNode(Tree):
@@ -28,7 +33,8 @@ class NumNode(Tree):
 class Calc:
     def __init__(self, input_string: str = ""):
         self._input = input_string
-        self._tokens: List[Bracket | Op | Number] = []
+        self._tokens: TokensList = []
+        self._grouped_tokens: GroupedTokensList = []
         self._tree: Tree | None = None
         self._value: Number | None = None
         self._is_evaluated = False
@@ -42,6 +48,7 @@ class Calc:
         if input_string != self._input:
             self._input = input_string
             self._tokens = []
+            self._grouped_tokens = []
             self._tree = None
             self._value = None
             self._is_evaluated = False
@@ -87,8 +94,9 @@ class Calc:
             gap_text = text[pos:end_pos]
             invalid_text_match = self._non_whitespace_re.search(gap_text)
             if invalid_text_match:
-                raise ValueError(
-                    f"unexpected text at character {pos + invalid_text_match.start() + 1}: "
+                raise SyntaxError(
+                    (34 + pos + invalid_text_match.start()) * " " + "^\n"
+                    f"unexpected text at {pos + invalid_text_match.start() + 1}: "
                     f"'{invalid_text_match[0]}'"
                 )
 
@@ -114,6 +122,7 @@ class Calc:
                 val = self._sym_tokens[match_text]
             else:
                 raise NotImplementedError(
+                    (34 + match.start()) * " " + "^\n"
                     f"unrecognized token at character {match.start()}: '{match_text}'"
                 )
             self._tokens.append((val, match.start(), match.end()))
@@ -123,8 +132,47 @@ class Calc:
 
         _check_for_illegal_text_between(pos, len(text))
 
+    _bracket_matching = {
+        Bracket.P_CLOSE: Bracket.P_OPEN,
+        Bracket.S_CLOSE: Bracket.S_OPEN,
+        Bracket.C_CLOSE: Bracket.C_OPEN,
+    }
+
+    def _group_by_brackets(self):
+        self._grouped_tokens = []
+        group, bracket = self._grouped_tokens, None
+
+        stack: List[Tuple[GroupedTokensList, Token]] = []
+        stack.append((group, bracket))
+
+        for token in self._tokens:
+            t, start, _ = token
+            if isinstance(t, (Number, Op)):
+                group.append(token)
+            else:
+                if t in (Bracket.P_OPEN, Bracket.S_OPEN, Bracket.C_OPEN):
+                    group, bracket = [], token
+                    stack[-1][0].append(group)
+                    stack.append((group, bracket))
+                else:  # Bracket.P_CLOSE | Bracket.S_CLOSE | Bracket.C_CLOSE
+                    if not bracket or self._bracket_matching[t] != bracket[0]:
+                        raise SyntaxError(
+                            (34 + start) * " " + "^\n"
+                            f"unmatched '{t.value}' at {start}"
+                        )
+                    else:
+                        stack.pop()
+                        group, bracket = stack[-1]
+
+        if len(stack) > 1:
+            raise SyntaxError(
+                (34 + bracket[1]) * " " + "^\n"
+                f"unmatched '{bracket[0].value}' at {bracket[1]}"
+            )
+
     def _build_tree(self):
         self._tokenize()
+        self._group_by_brackets()
 
     def _eval(self):
         self._build_tree()
@@ -148,11 +196,12 @@ def main():
             calc.input = s
 
             try:
-                calc._tokenize()
+                calc._build_tree()
                 print(f"Tokens: {[(str(t[0]), t[1], t[2]) for t in calc._tokens]}")
+                print(f"Grouped tokens: {calc._grouped_tokens}")
                 print(f"> {calc.result}")
-            except ValueError as ve:
-                print(ve)
+            except SyntaxError as se:
+                print(se)
 
             s = input("\nInput the expression to evaluate: ").strip()
         else:
